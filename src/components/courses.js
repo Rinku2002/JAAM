2f9fdccdae2e233bef9d87a935f252931f07087e
Rinku2002 <gangishettyrinku@gmail.com>
2022-06-07 20:15:06 +0530

    *

--- src/components/courses.js
+++ src/components/courses.js
@@ -14,10 +14,14 @@ function Courses() {
     let ui=useSelector(state=>state.ui)
     let navigate=useNavigate()
     let [dd,setdd]=useState([])
+    let [state,changeState]=useState(true)
     useEffect(() => {
         fetch('/courses-api/courses')
         .then(response => response.json())
-        .then(data => setdd(data.payload))
+        .then(data => {
+            setdd(data.payload)
+            changeState(false)
+        })
       },[])
       function funcc(v){
           let actionObj=addcinfo(v)
@@ -26,22 +30,29 @@ function Courses() {
     console.log(dd)
     return(
         <>
-        {ui.length==0 ?
+        {state?
         <div>
-            <p className='display-1 fw-bolder text-danger text-center login mt-5'>Login</p>
-            <p className='display-3 text-center login'>To access the courses</p>
-            <button onClick={()=>navigate('/login')} className='btn btn-success d-block mx-auto'>Login</button>
-            <img className='w-lg-25 w-sm-50 w-50 d-block mx-auto mt-3' src={loginsvg} />
+            <p>Loading</p>
         </div>:
-        <div className='row mt-5 justify-content-start'>
-        {
-        dd.map((v,index)=>
-            <Link onClick={()=>funcc(v)} to="/course" key={index} className='col col-lg-3 col-md-3 col-sm-12 col-12 text-center border border-5 border-info bg-light pt-4 rounded rounded-5 course'>
-                <img src={v.tnumbnail} className='w-100 h-75' />
-                <p className='mb-0 text-dark'>{v.name}</p>
-            </Link>
-        )
-        }
+        <div>
+            {ui.length==0 ?
+            <div>
+                <p className='display-1 fw-bolder text-danger text-center login mt-5'>Login</p>
+                <p className='display-3 text-center login'>To access the courses</p>
+                <button onClick={()=>navigate('/login')} className='btn btn-success d-block mx-auto'>Login</button>
+                <img className='w-lg-25 w-sm-50 w-50 d-block mx-auto mt-3' src={loginsvg} />
+            </div>:
+            <div className='row mt-5 justify-content-start'>
+            {
+            dd.map((v,index)=>
+                <Link onClick={()=>funcc(v)} to="/course" key={index} className='col col-lg-3 col-md-3 col-sm-12 col-12 text-center border border-5 border-info bg-light pt-4 rounded rounded-5 course'>
+                    <img src={v.tnumbnail} className='w-100 h-75' />
+                    <p className='mb-0 text-dark'>{v.name}</p>
+                </Link>
+            )
+            }
+            </div>
+            }
         </div>
         }
         </>
